2ef01cec9fbd57f7845beddeef4d415853d74960
shankar-01 <shankar.bscsf19@iba-suk.edu.pk>
2023-06-24 15:45:54 +0500

    read from db api added

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,25 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/ead').then(()=>console.log("DB connected")).catch((err)=>console.log("DB connect ", err))
 const Receipe = require('./models/Receipe')
 
+//get image
+app.get('/uploads/:_filename', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'uploads', req.params._filename))
+    //res.end()
+})
+
+//get receipes
+app.get('/receipes', async (req, res)=>{
+    const data = await Receipe.find()
+    res.send(data)
+})
+
+//get receipe b y id
+
+app.get('/receipe/:_id', async (req, res)=>{
+    const {_id} = req.params
+    const data = await Receipe.findById(_id)
+    res.send(data)
+})
 
 //add validation middleware
 const validation = require('./middleware/validate')
